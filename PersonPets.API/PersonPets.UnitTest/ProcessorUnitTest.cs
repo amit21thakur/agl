9fc0508a1673fcfe2d5a7d144808687e6a2f6fda
Amit Kumar <amit21thakur@gmail.com>
2020-08-27 08:58:17 +1000

    Adding another test project for unit tests

---
+++ PersonPets.API/PersonPets.UnitTest/ProcessorUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersonPets.UnitTest
+{
+    [TestClass]
+    public class ProcessorUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PersonPets.API/PersonPets.UnitTest/ValidatorUnitTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonPets.UnitTest
+{
+    class ValidatorUnitTest
+    {
+    }
+}
